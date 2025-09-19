49b1b804ff6039c1bd25e13e93eac9d30a49cef7
Romi Setiawan <romi36825@gmail.com>
2025-09-19 22:23:26 +0700

    Fix Midtrans

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-    $middleware->alias([
-        'role' => \App\Http\Middleware\CheckRole::class,
-    ]);
+        // Middleware alias yang sudah ada sebelumnya
+        $middleware->alias([
+            'role' => \App\Http\Middleware\CheckRole::class,
+        ]);
+
+        // Menambahkan pengecualian CSRF untuk callback Midtrans
+        $middleware->validateCsrfTokens(except: [
+            'midtrans/callback',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
