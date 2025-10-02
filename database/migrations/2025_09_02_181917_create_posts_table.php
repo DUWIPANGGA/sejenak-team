93982b6f9e17657a701ce86a06f2aa66d292ea3e
DUWIPANGGA <aryad2232@gmail.com>
2025-10-03 01:41:58 +0700

    new

--- database/migrations/2025_09_02_181917_create_posts_table.php
+++ database/migrations/2025_09_02_181917_create_posts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('tittle');
+            $table->string('title');
             $table->text('content');
             $table->string('image')->nullable();
             $table->boolean('is_anonymous')->default(false);
