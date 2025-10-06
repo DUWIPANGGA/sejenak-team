2fb23aebc5a6d509243118e731d38c4d32890013
DUWIPANGGA <aryad2232@gmail.com>
2025-10-07 02:58:39 +0700

    new

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
-class PostApiController extends Controller
+class PostController extends Controller
 {
     /**
      * Ambil semua postingan
