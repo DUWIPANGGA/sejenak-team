943e0b2023f1f9e87fd7c1380453845efed6eed8
DUWIPANGGA <aryad2232@gmail.com>
2025-10-07 02:55:30 +0700

    new

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\LikeController;
 use App\Http\Controllers\Api\MoodController;
